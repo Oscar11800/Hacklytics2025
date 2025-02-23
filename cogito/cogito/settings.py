35d8bfb8f05cd94b0b6957929c398687a8a6f27e
Oscar Chen <oscar1149412@gmail.com>
2025-02-23 01:07:48 -0500

    more vercel edits

--- cogito/cogito/settings.py
+++ cogito/cogito/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-85udxaqj^k341&pkn-vh@ivygy06uxi-ucs5(s1t^32u%8x#w(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel-app', '127.0.0.1']
+ALLOWED_HOSTS = ['protein-ai-iota.vercel-app', '127.0.0.1', 'proteinai.tech', 'www.proteinai.tech']
 
 
 # Application definition
