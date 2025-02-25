a3b96fde37a2a58e37b3c150a5a9b36ab88a1f71
Oscar Chen <oscar1149412@gmail.com>
2025-02-24 23:17:43 -0600

    Update settings.py

--- cogito/cogito/settings.py
+++ cogito/cogito/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'protein',
     'home',
-
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -49,8 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 ROOT_URLCONF = 'cogito.urls'
 
 TEMPLATES = [
